072677b4342831dc8dbeb448551f9b549af0534b
Umar <syedumarusman@mail.adelphi.edu>
2021-11-10 17:55:28 -0500

    implemented new speech to text class

--- flask/main.py
+++ flask/main.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from transcribeAudio.googleSpeechToText import LongSpeechToText
 from summarize.summarization import Summarization
+from flask_cors import CORS
+import os
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "transcribeAudio/input-audios")
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
 api = Api(app)
 
 api.add_resource(LongSpeechToText, '/transcribe/')
--- flask/transcribeAudio/googleSpeechToText.py
+++ flask/transcribeAudio/googleSpeechToText.py
@@ -1,74 +1,95 @@
-from google.cloud import speech_v1p1beta1 as speech
-from flask_restful import Resource, reqparse
-import io, os
+from google.cloud import speech_v1p1beta1 as speech, storage
+from google.protobuf.json_format import MessageToJson, MessageToDict
+from flask_restful import Resource, request
+from flask import current_app as app
+from pydub import AudioSegment
+from werkzeug.utils import secure_filename
+import wave
+import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "auth.json"
 
 class LongSpeechToText(Resource):
+    bucket_name = "capstone-audio-files"
+    # UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "input-audios")
 
     def __init__(self):
         super().__init__()
         # Instantiates a client
         print("Speech to Text Module\n")
 
-    # transcribes long audio file from cloud (gcs_uri) into text
-    def get(self):
-        # gcs_uri: str, speakerCount: int = 2
-        parser = reqparse.RequestParser()
-        parser.add_argument('gcs_uri', type=str, required=True, help="gcs_uri cannot be empty")
-        parser.add_argument('speakerCount', type=int)
-        args = parser.parse_args()
-        gcs_uri = args['gcs_uri']
-        speakerCount = args['speakerCount']
-        if speakerCount == None:
-            speakerCount = 2
+    def upload_blob(self, bucket_name, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+    
+    def stereo_to_mono(self, audio_file_name):
+        sound = AudioSegment.from_wav(audio_file_name)
+        sound = sound.set_channels(1)
+        sound.export(audio_file_name, format="wav")
+
+    def frame_rate_channel(self, audio_file_name):
+        with wave.open(audio_file_name, "rb") as wave_file:
+            frame_rate = wave_file.getframerate()
+            channels = wave_file.getnchannels()
+            return frame_rate,channels
+
+    def get_blob_url(self, bucket_name, blob_name):
+        return f'gs://{bucket_name}/{blob_name}'
+
+    def post(self):
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        frame_rate, channels = self.frame_rate_channel(file_path)
+
+        if channels > 1:
+            self.stereo_to_mono(file_path)
+
+        # upload blob to cloud
+        self.upload_blob(self.bucket_name, file_path, filename)
+
+        # get gcs_uri of the audio file (blob) 
+        gcs_uri = self.get_blob_url(self.bucket_name, filename)
+
+        # Initialize speech client
         client = speech.SpeechClient()
         audio = speech.RecognitionAudio(uri=gcs_uri)
+
+        # Setting speech recognition config
         config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-            # sample_rate_hertz=44100,
-            language_code="en-US",
-            # enable_word_time_offsets= True,
-            # enable_automatic_punctuation= True,
-            audio_channel_count= 2,
-            enable_separate_recognition_per_channel= True,
-            # enable_word_confidence=True,
+            sample_rate_hertz= frame_rate,
+            language_code= 'en-US',
             enable_speaker_diarization= True,
-            diarization_speaker_count= speakerCount,
-        )
+            enable_automatic_punctuation= True,
+            diarization_speaker_count= 2)
 
+        # Detects speech in the audio file
         operation = client.long_running_recognize(config=config, audio=audio)
-
-        print('Waiting for operation to complete...')
-        response = operation.result(timeout=90)
-
+        response = operation.result(timeout=10000)
+        # res = MessageToJson(response)
         result = response.results[-1]
-
         words_info = result.alternatives[0].words
+        print(words_info)
 
-        sentenceInfo = []
-        speaker = words_info[0].speaker_tag
-        sentenceNo = 1
-        for word_info in enumerate(words_info, 1):
-            currentSpeaker = word_info[1].speaker_tag
-            currentWord = word_info[1].word
-            if(speaker == currentSpeaker):
-                key = 'speaker'+str(currentSpeaker)+'_sentence'+str(sentenceNo)
-                flagList = [i for i,x in enumerate(sentenceInfo) if key in x]
-                if len(flagList) > 0:
-                    newWord = " " + currentWord
-                    sentenceInfo[ flagList[0] ][key] += newWord
-                else:
-                    sentenceObj = {}
-                    sentenceObj[key] = currentWord
-                    sentenceInfo.append(sentenceObj)
-            else:
-                sentenceNo+=1
-                key = 'speaker'+str(currentSpeaker)+'_sentence'+str(sentenceNo)
-                speaker = currentSpeaker
-                sentenceObj = {}
-                sentenceObj[key] = currentWord
-                sentenceInfo.append(sentenceObj)
+        # speakerTag = 1
+        # sentence = ""
+        # transcript = ''
 
-        return sentenceInfo
-    # [END speech_transcribe_async_gcs]
+        # for word_info in words_info:
+        #     if word_info.speaker_tag == speakerTag:
+        #         sentence = sentence + " " + word_info.word
+        #     else:
+        #         transcript += "speaker {}: {}".format(speakerTag, sentence) + '\n'
+        #         speakerTag = word_info.speaker_tag
+        #         sentence = "" + word_info.word
+    
+        # transcript += "speaker {}: {}".format(speakerTag,sentence)
+        # print(transcript)
+        # response = { gcs_uri, transcript }
+        
+        # return res
